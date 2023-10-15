d2c7352994c752545c144669da1100c22c6c85ba
sarowley <sarowley@ucsc.edu>
2023-10-15 16:46:24 -0700

    finished step 1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
-
 const gameName = "Sticker Sketchpad";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
